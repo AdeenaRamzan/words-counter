d20f6128b8242c9b59d4043b7557a3a6998b1fff
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-04-04 12:09:26 +0500

    Added words-counter

---
+++ main.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const paragraph = await inquirer.prompt([
+  {
+    name: "paragraph",
+    type: "input",
+    message: "Enter a paragraph: ",
+  },
+  {
+    name: "options",
+    type: "list",
+    message: "Select any one option:",
+    choices: ["Count characters", "Count words"],
+  },
+]);
+if (paragraph.options === "Count characters") {
+  const characters = paragraph.paragraph.trim().replace(/\s+/g, "");
+
+  console.log(`Total characters in your paragraph: ${characters.length}`);
+} else if (paragraph.options === "Count words") {
+  const words = paragraph.paragraph.trim().split(" ");
+  console.log(`Total words in your paragraph: ${words.length}`);
+}
